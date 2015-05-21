5ac98d1424e0eca03e3b7c88deeb101a0549a359
Henry Phan <hphan.cs@gmail.com>
2015-05-21 00:34:32 -0400

    Added .top_tag, .page(n), and .count_tagged_by(tags) functions, with output results in plain text files

--- lib/blergers.rb
+++ lib/blergers.rb
@@ -9,11 +9,59 @@ module Blergers
   class Post < ActiveRecord::Base
     has_many :post_tags
     has_many :tags, through: :post_tags
+
+    def self.post(n)
+      self.order(date: :desc).limit(10).offset((n-1)*10)
+    end
+
+## count_tagged_by(tags) where the tag parameter is an array of tag strings
+    def self.count_tagged_by(tags)
+      tag_counts = []
+      # select("id")
+      tag_ids = []
+      tags.each do |x|
+        t_id = Blergers::Tag.where(name: x).pluck(:id)
+        tag_ids << t_id[0]
+      end
+      tag_ids.each do |x|
+        tag_count = Blergers::PostTag.where("tag_id == #{x}").count
+        tag_name = Blergers::Tag.where(id: x).pluck(:name)
+        tag_name = tag_name[0]
+        tag_counts << [tag_count, tag_name]
+      end
+      puts "** Post Tagged Count Search Query **"
+      tag_counts.each do |tag_count, tag_name|
+        puts "#{tag_name} -> #{tag_count}"
+      end
+    end
   end
 
   class Tag < ActiveRecord::Base
     has_many :post_tags
     has_many :posts, through: :post_tags
+
+    attr_reader :tag_counts
+
+    def self.tag_total
+      @tag_counts = []
+      self.all.each do |x|
+        t_id = x.id
+        tag_count = Blergers::PostTag.where("tag_id == #{x.id}").count
+        tag_name = self.where(id: x.id).pluck(:name)
+        tag_name = tag_name[0]
+        @tag_counts << [tag_count, tag_name]
+      end
+      @tag_counts
+    end
+
+    def self.top_tags
+      self.tag_total
+      tag_counts = @tag_counts.sort.reverse.each_slice(10).to_a
+      puts "** Top 10 Most Used Tags **"
+      tag_counts[0].each do |tag_count, tag_name|
+        puts "#{tag_name} -> #{tag_count}"
+      end
+    end
   end
 
   class PostTag < ActiveRecord::Base
